8d36c313a63c73160543975fb2f0c18ffcbf2a11
Andrew Sun <me@andrewsun.com>
2019-09-14 23:28:06 -0400

    What if we tried more power?

--- examples/textgenrnn/run.py
+++ examples/textgenrnn/run.py
@@ -1,6 +1,6 @@
 from textgenrnn import textgenrnn
 
 textgen = textgenrnn()
-textgen.train_from_file('datasets/reddit_apple_android_2000.txt', num_epochs=2, new_model=True)
+textgen.train_from_file('datasets/reddit_apple_android_2000.txt', num_epochs=2, new_model=True, batch_size=1024)
 textgen.generate()
 
