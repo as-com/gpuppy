e4ba3a96f9f2ea6d51d9e942419ebf728b5d5e20
Andrew Sun <me@andrewsun.com>
2019-09-14 21:48:53 -0400

    What if we tried more power?

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {JobList} from "./JobList";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>GPUppy</h1>
+        <p><i>What if we tried more power?</i></p>
+        <JobList />
       </header>
     </div>
   );
---
+++ frontend/src/JobList.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {Job} from "./job";
+
+export class JobList extends React.Component<{}, {
+    jobs: Job[] | null
+}> {
+
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            jobs: null
+        }
+    }
+
+    componentDidMount() {
+        this.fetchStuff();
+        setInterval(x => {
+            this.fetchStuff();
+        }, 2000);
+    }
+
+    fetchStuff() {
+        fetch("/api/jobs").then(r => r.json())
+            .then(x => {
+                this.setState({
+                    jobs: x.jobs
+                })
+            });
+    }
+
+    render() {
+        let jobs = this.state.jobs;
+        if (jobs === null) {
+            return <div>Loading...</div>
+        } else {
+            return <div>
+                <table className={"jobs"}>
+                    <tbody>
+                    <tr>
+                        <th>ID</th>
+                        <th>Filename</th>
+                        <th>Command</th>
+                        <th>Status</th>
+                    </tr>
+                    {jobs.map(x => <tr key={x._id}>
+                        <td>{x._id}</td>
+                        <td>{x.filename}</td>
+                        <td>{x.command}</td>
+                        <td>{x.status}</td>
+                    </tr>)}
+                    </tbody>
+                </table>
+            </div>
+        }
+    }
+}
